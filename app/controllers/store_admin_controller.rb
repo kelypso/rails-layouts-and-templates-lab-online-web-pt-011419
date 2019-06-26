47d4c3b7812fcbf08380b0869316bbe1aba73380
Kelsey Low <kklowlow@gmail.com>
2019-06-26 02:42:10 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,11 +1,11 @@
 class StoreAdminController < ApplicationController
-  layout => "admin"
+  layout "admin"
 
   def home
   end
 
   def orders
-    render :layout => "order_administration"
+    render :layout => 'order_administration'
   end
 
   def invoice
